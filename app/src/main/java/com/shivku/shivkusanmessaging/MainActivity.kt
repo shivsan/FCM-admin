9a96bd9c50fa585c0ebe015de1070afda32e7b5c
Shivku <sivakumar.sekar@rakuten.com>
2020-08-31 11:21:52 +0530

    First working commit

---
+++ app/src/main/java/com/shivku/shivkusanmessaging/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.shivku.shivkusanmessaging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    val msg = "Token " + token
+                    Log.d(TAG, msg)
+                })
+    }
+
+    companion object {
+
+        private const val TAG = "MainActivity"
+    }
+}
